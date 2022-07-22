885e38623022a2f1dded0e54d6b15a8c08f0f538
Theo Kaufman <tpkaufman@gmail.com>
2022-07-22 17:54:39 -0400

    working on visualizations

--- frontend/components/Charts/Bar.js
+++ frontend/components/Charts/Bar.js
@@ -59,7 +59,7 @@ const BarChart = () => {
 
 return(
     <div>
-        <Bar 
+      <Bar 
         data = {data}
         options = {options} 
         height = {400}
--- frontend/components/Charts/Line.js
+++ frontend/components/Charts/Line.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
-import {Chart as ChartJS, LinearScale, LineElement, CategoryScale, Legend, PointElement}  from 'chart.js'
+import {Chart as ChartJS, LinearScale, LineElement, CategoryScale, Legend, PointElement, scaleService}  from 'chart.js'
 import React, {useEffect, useState} from 'react';
-import {Line} from 'react-chartjs-2'
+import {Line} from 'react-chartjs-2';
+import useLines from '/hooks/useLines.jsx';
 
 ChartJS.register(
     LinearScale,
@@ -21,7 +22,6 @@ const LineChart = () => {
         async function getData() {
             try {
                 const response = await axios.get(baseURL);
-                console.log(response)
                 setChart(response.data)
             } catch (error) {
                 console.log(error)
@@ -30,10 +30,15 @@ const LineChart = () => {
         getData()
     }, [baseURL])
 
+    let gradeArray = ["g01", "g02", "g03"]
+
+    let lines = useLines(gradeArray, chart)
+
+    console.log(lines)
 
     const linedata = 
         {
-            labels: chart.map(x => x.year),
+            labels: [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021],
             datasets: [{
                 label: "Test",
                 data: chart.map(x => x.seg_index_tot),
@@ -44,7 +49,7 @@ const LineChart = () => {
                     'rgb(255, 99, 132)'
                   ],
                   borderWidth: 1
-            }]
+            }] 
         }
 
     const options = {
