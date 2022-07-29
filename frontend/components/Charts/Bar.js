5db19077dfb1699c57b20507e9a6bb65e481ed11
Theo Kaufman <tpkaufman@gmail.com>
2022-07-29 17:35:45 -0400

    adding select dropdown options

--- frontend/components/Charts/Bar.js
+++ frontend/components/Charts/Bar.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import {Chart as ChartJS, LinearScale, BarElement, CategoryScale, Legend}  from 'chart.js'
 import React, {useEffect, useState} from 'react';
-import {Bar} from 'react-chartjs-2'
+import {Bar} from 'react-chartjs-2';
+import Select from 'react-select';
+import {StateOption, YearOption} from './Options';
 
 ChartJS.register(
     LinearScale,
@@ -12,29 +14,42 @@ ChartJS.register(
 
 const BarChart = () => {
 
-    const [state, setState] = useState("TX")
+  // Select Component
+    
+    // mapping to 
+    const selectoptions = StateOption.map(d => ({
+      "value": d.abbreviation,
+      "label": d.name
+    }) 
+    )  
 
-    const [chart, setChart] = useState([]);
+    // Initializing state and year state variables
+    const [state, setState] = useState();
 
+    const [year, setYear] = useState();
+ 
 
-    let baseURL = "http://localhost:8000/api/state/?year=2020&state_abb="
+    // Initializing chart state variable
+    const [chart, setChart] = useState([]); 
+
+    let baseURL = "http://localhost:8000/api/state/?"
 
     useEffect(() => {
         async function getData() {
             try {
-                const response = await axios.get(baseURL + state);
+                const response = await axios.get(baseURL + "year=" + year + "&state_abb=" + state);
                 setChart(response.data)
             } catch (error) {
                 console.log(error)
             }
         }
         getData()
-    }, [baseURL, state])
+    }, [baseURL, state, year])
 
     const data = {
         labels: chart?.map(x => x.grade),
         datasets: [{
-          label: "2020 Segregation index totals",
+          label: year + " Segregation index totals",
           data: chart?.map(x => x.seg_index_tot), 
           backgroundColor: ['rgba(255, 99, 132, 0.2)'],
           borderColor: ['rgb(255, 99, 132)'],
@@ -58,8 +73,14 @@ const BarChart = () => {
 
 return(
     <div>
-      <button onClick={() => {setState("TX")}}>Texas</button>
-      <button onClick={() => {setState("NY")}}>New York</button>
+      <Select 
+        options={selectoptions}
+        onChange={e => {setState(e.value)}}
+        placeholder="Select a State"/>
+      <Select 
+        options={YearOption}
+        onChange={e => {setYear(e.value)}}
+        placeholder="Select a Year"/>
       <Bar 
         data = {data}
         options = {options} 
