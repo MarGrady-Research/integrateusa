f386e5d02eb9fe979adb564ffad6982a40ec209f
Theo Kaufman <tpkaufman@gmail.com>
2022-09-01 19:58:04 -0400

    Adding selections and working on name searching

--- frontend/components/Selection.js
+++ frontend/components/Selection.js
@@ -1,66 +1,103 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
+import AsyncSelect from 'react-select/async';
+import axios from 'axios';
+import {years, grades} from './SelectOptions';
+import { handleInputChange } from 'react-select';
 
 function Selection() {
 
-  const [visible, setVisible] = useState(false);
+  // Defining level state variable and levelselect array
+  const [levels, setLevels] = useState(-1);
 
-  const levels = [
-    {value: "district", label: "District"},
-    {value: "county", label: "County"},
-    {value: "state", label: "State"}
+  const levelselect = [
+    {value: 0, label: "District", route: "api/districtnames/?search="},
+    {value: 1, label: "County", route: "api/countynames/?search="},
+    {value: 2, label: "State", route: "api/statenames/?search="}
   ]
 
-  const years = [
-    {value: 2000, label: 2000},
-    {value: 2001, label: 2001},
-    {value: 2002, label: 2002},
-    {value: 2003, label: 2003},
-    {value: 2004, label: 2004},
-    {value: 2005, label: 2005},
-    {value: 2006, label: 2006},
-    {value: 2007, label: 2007},
-    {value: 2008, label: 2008},
-    {value: 2009, label: 2009},
-    {value: 2010, label: 2010},
-    {value: 2011, label: 2011},
-    {value: 2012, label: 2012},
-    {value: 2013, label: 2013},
-    {value: 2014, label: 2014},
-    {value: 2015, label: 2015},
-    {value: 2016, label: 2016},
-    {value: 2017, label: 2017},
-    {value: 2018, label: 2018},
-    {value: 2019, label: 2019},
-    {value: 2020, label: 2020},
-    {value: 2021, label: 2021}
-    ]
+  // Setting baseURL based on level
+  const setURL = () => {
+  if (levels > -1) {
+    return "http://localhost:8000/" + levelselect[levels].route
+    }
+  }
 
-  const grades = [
-    {value: "PK", label: "PK"},
-    {value: "KG", label: "KG"},
-    {value: "01", label: "01"},
-    {value: "02", label: "02"},
-    {value: "03", label: "03"},
-    {value: "04", label: "04"},
-    {value: "05", label: "05"},
-    {value: "06", label: "06"},
-    {value: "07", label: "07"},
-    {value: "08", label: "08"},
-    {value: "09", label: "09"},
-    {value: "10", label: "10"},
-    {value: "11", label: "11"},
-    {value: "12", label: "12"}
-  ]
+  let baseURL = setURL()
 
+  // defining input state
+  const [input, setInput] = useState('')
+
+  // Function to handle inputs into searchbox
+  handleInputChange = (inputValue) => {
+    setInput(inputValue);
+    console.log(inputValue)
+  }
+
+  // defining names state variable (array to hold name data)
+  const [names, setNames] = useState([])
+
+  // async function returning a promise for name data
+  const nameOptions = async () => {
+      if (input != undefined && input != '') {
+        try {
+          const response = await axios.get(baseURL + input);
+          setNames(response.data);
+          let nameOptions = names.map(d => ({
+              "value": d.county_id,
+              "label": d.county_name
+            }
+            ))
+          return nameOptions
+        }
+        catch(error) {
+          console.log(error)
+        }
+    }
+  }
+
+
+// Returning JSX
   return (
     <div className='relative w-full'>
-      <div className='flex flex-row ml-20 mt-20'>
-        <span className='text-3xl mt-100 mr-5'>Select a </span>
+      <div className='flex flex-row ml-20 mt-10'>
+        <p className='text-3xl mt-100 mr-5'>Select a </p>
         <Select 
-        options = {levels}
+        options = {levelselect}
         placeholder = "Geographic Level"
+        onChange= {e => {setLevels(e.value)}} 
+        components={{IndicatorSeparator: () => null}}
         />
+      </div>
+      <div className='flex flex-row ml-20 mt-5'>
+      {levels > -1 &&
+        <>
+        <AsyncSelect 
+        cacheOptions
+        defaultOptions
+        loadOptions={nameOptions}
+        onInputChange={handleInputChange}
+        components={{ DropdownIndicator: () => null, IndicatorSeparator: () => null }}
+        placeholder= {"Type a " + levelselect[levels].label + " name"}
+        /> 
+        <Select 
+        options={years}
+        placeholder="Select a year"
+        name='years'
+        className='ml-5'
+        />
+        <Select 
+        options={grades}
+        placeholder="Select a grade"
+        isMulti
+        name='grades'
+        className='ml-5'
+        />
+      </>
+      }
+      </div>
+      <div>
+        
       </div>
     </div>
   );
