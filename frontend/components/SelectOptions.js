e1a8c1dd7c9477f18d24f07d22e54b410d1ffda5
Theo Kaufman <tpkaufman@gmail.com>
2022-09-01 19:58:18 -0400

    separate file for year and grade options

---
+++ frontend/components/SelectOptions.js
@@ -0,0 +1,46 @@
+// Defining year options
+  const years = [
+    {value: 2000, label: 2000},
+    {value: 2001, label: 2001},
+    {value: 2002, label: 2002},
+    {value: 2003, label: 2003},
+    {value: 2004, label: 2004},
+    {value: 2005, label: 2005},
+    {value: 2006, label: 2006},
+    {value: 2007, label: 2007},
+    {value: 2008, label: 2008},
+    {value: 2009, label: 2009},
+    {value: 2010, label: 2010},
+    {value: 2011, label: 2011},
+    {value: 2012, label: 2012},
+    {value: 2013, label: 2013},
+    {value: 2014, label: 2014},
+    {value: 2015, label: 2015},
+    {value: 2016, label: 2016},
+    {value: 2017, label: 2017},
+    {value: 2018, label: 2018},
+    {value: 2019, label: 2019},
+    {value: 2020, label: 2020},
+    {value: 2021, label: 2021}
+    ]
+
+  // defining grade options
+  const grades = [
+    {value: "PK", label: "PK"},
+    {value: "KG", label: "KG"},
+    {value: "01", label: "01"},
+    {value: "02", label: "02"},
+    {value: "03", label: "03"},
+    {value: "04", label: "04"},
+    {value: "05", label: "05"},
+    {value: "06", label: "06"},
+    {value: "07", label: "07"},
+    {value: "08", label: "08"},
+    {value: "09", label: "09"},
+    {value: "10", label: "10"},
+    {value: "11", label: "11"},
+    {value: "12", label: "12"}
+  ]
+
+
+  export {years, grades}
