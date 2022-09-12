d37c865033ff5415c2d6622a1cc0cd22eea0f363
Theo Kaufman <tpkaufman@gmail.com>
2022-09-12 17:15:46 -0400

    Adding header and footer

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -4,47 +4,35 @@ import Image from "next/image";
 
 function Header() {
 
-    return(
-        <div className="relative h-400">
-        <div className="relative h-300 mt-10">
-            <div className="flex flex-row justify-evenly">
-                <a href="https://www.margrady.com"
-                   className="flex justify-start basis-1/4 ml-20">
-                <Image src="/mg_logo.png" 
-                       alt="margrady logo" 
-                       width={100}
-                       height={30}/>
-                </a>
-                <h1 className="flex justify-center text-4xl basis-1/2">IntegrateUSA</h1>
-                <a href="https://www.carnegie.org/"
-                   className="flex justify-end basis-1/4 mr-20">
-                    <Image 
-                        src="/ccny_logo.svg" 
-                        alt="ccny logo" 
-                        width={100} 
-                        height={30}
-                    />
-                </a>
-            </div> 
-        </div>
-        <div className="h-500 mt-10">
-            <div className="static flex justify-evenly text-3l">
-                <Link href="/info">
-                    <a className="hover:underline active:underline">Info and Trends</a>
-                </Link>
-                <Link href="/segregation">
-                    <a className="hover:underline">Segregation Measures</a>
-                </Link>
-                <Link href="/map">
-                    <a className="hover:underline">Map</a>
-                </Link>
-                <Link href="/about">
-                    <a className="hover:underline">About</a>
-                </Link>
-            </div>
-    </div>
-    </div>
-    )
+// Note: Header code adapted from https://tailblocks.cc/
+
+return(
+    <header className="text-gray-600 bg-gray-300 body-font">
+  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <Image src="/mg_logo_cropped.png" 
+                       alt="margrady logo"
+                       width = {50}
+                       height={50}/>
+      <span className="ml-3 text-xl">IntegrateUSA</span>
+    </a>
+    <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+    <Link href="/info">
+    <a className="mr-5 hover:text-gray-900">Info and Trends</a>
+    </Link>
+    <Link href="/segregation">
+    <a className="mr-5 hover:text-gray-900">Segregation</a>
+    </Link>
+    <Link href="/map">
+    <a className="mr-5 hover:text-gray-900">Map</a>
+    </Link>
+    <Link href="/about">
+    <a className="mr-5 hover:text-gray-900">About</a>
+    </Link>
+    </nav>
+  </div>
+</header>
+)
 }
 
 export default Header
--- frontend/pages/info.js
+++ frontend/pages/info.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import Selection from "../components/Selection";
 
 export default function Info(){
     return(
-        <div>
-        <Header />
-        <Selection />
+        <>
+        <Header/>
+        <div className="flex flex-col min-h-screen">
+        <Selection/>
+        <Footer />
         </div>
+        </>
     )
 }
