dca2fdd3fae0702b04d5573441f1eec15e665653
Theo Kaufman <tpkaufman@gmail.com>
2022-09-14 16:31:41 -0400

    changing query filtering

--- backend/views.py
+++ backend/views.py
@@ -48,19 +48,13 @@ class countyList(generics.ListAPIView):
 class countyNameList(generics.ListAPIView):
     queryset = CountyNames.objects.all()
     serializer_class = CountyNameSerializer
-    # context_object_name = "county_name"
-    # filter_backends = [filters.SearchFilter]
-    # search_fields = [
-    #     'county_id',
-    #     '^county_name',
-    #     'vector_col'
-    # ]
+    context_object_name = "county_name"
 
     def get_queryset(self):
         query = self.request.GET.get("q")
         # search_vector = search.SearchVector('county_name')
-        search_query = search.SearchQuery(query + ':*', search_type='raw')
-        return CountyNames.objects.filter(vector_col=search_query).order_by('county_name')
+        # search_query = search.SearchQuery(query) #+ ':*', search_type='raw')
+        return CountyNames.objects.filter(county_name__istartswith = query)
 
 class stateList(generics.ListAPIView):
     queryset = StateSeg.objects.all()
