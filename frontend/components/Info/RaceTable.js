5a5a206cd936721fce09d2b48e0a49d9d3f9c3fe
Theo Kaufman <tpkaufman@gmail.com>
2022-09-16 17:44:22 -0400

    Improving table code

--- frontend/components/Info/RaceTable.js
+++ frontend/components/Info/RaceTable.js
@@ -1,40 +1,51 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
+
 
 export default function RaceTable({enrGroups, enrTotal}) {
 
     const tableRows = (arr) => {
         return arr.map(e => {
          return(
-                 <tr key={e.group}>
-                 <td>{e.group.toUpperCase()}</td>
-                 <td>{e.enr}</td>
-                 <td>{Math.round((e.enr/enrTotal)*100)}</td>
+                 <tr key={e.group} className="border-b">
+                 <td className="text-sm text-gray-900 font-light px-6 py-4 ">{e.group.toUpperCase()}</td>
+                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{e.enr}</td>
+                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{Math.round((e.enr/enrTotal)*100)}</td>
                  </tr> 
          )
          })
      }
 
     return(
-    <table className="ml-10">
-                <thead>
-                <tr>
-                    <th>Group</th>
-                    <th>#</th>
-                    <th>%</th>
-                </tr>
-                </thead>
+        // <div className="flex flex-col border-2 border-black">
+            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8 ">
+                <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8 ">
+                    <div className="overflow-x-auto"> 
+                        <table className="min-w-full  ">
+                            <thead className="border-b bg-gray-200">
+                                <tr>
+                                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Group</th>
+                                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">#</th>
+                                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">%</th>
+                                </tr>
+                            </thead>
 
-                <tbody>
-                    {tableRows(enrGroups)}
-                </tbody>
-                
-                <tfoot>
-                <tr>
-                    <td>Total</td>
-                    <td>{enrTotal}</td>
-                    <td>100</td>
-                </tr>
-                </tfoot>
-            </table>
+                            <tbody>
+                                {tableRows(enrGroups)}
+                            </tbody>
+                            
+                            <tfoot className="border-b">
+                                <tr>
+                                    <td scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Total</td>
+                                    <td scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">{enrTotal}</td>
+                                    <td scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">100</td>
+                                </tr>
+                            </tfoot>
+
+                        </table>
+                    </div>
+                </div>
+             </div>
+        // </div>
+    
     )
 }
---
+++ frontend/components/Segregation/SegTable.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { useTable } from "react-table";
+
+
+export default function SegTable({SegData}) {
+
+    const tableRows = (arr) => {
+        return arr.map(e => {
+         return(
+                 <tr key={e.dist_id} className="border-b">
+                 <td className="text-sm text-gray-900 font-light px-6 py-4 ">{e.dist_name}</td>
+                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{e.enr_prop_as}</td>
+                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{e.enr_prop_bl}</td>
+                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{e.enr_prop_hi}</td>
+                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{e.enr_prop_wh}</td>
+                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{e.enr_prop_or}</td>
+                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{e.norm_exp_aw}</td>
+                 </tr> 
+         )
+         })
+     }
+
+     console.log(SegData);
+
+
+    return(
+        <>
+            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8 ">
+                <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8 ">
+                    <div className="overflow-x-auto"> 
+                        <table className="min-w-full">
+                            <thead className="border-b bg-gray-200">
+                                <tr>
+                                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">District Name</th>
+                                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">% Asian</th>
+                                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">% Black</th>
+                                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">% Hispanic</th>
+                                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">% White</th>
+                                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">% Other</th>
+                                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Noramlized Exposure</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {tableRows(SegData)}
+                            </tbody>
+                            
+                            {/* <tfoot className="border-b">
+                                <tr>
+                                    <td scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Total</td>
+                                    <td scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">{enrTotal}</td>
+                                    <td scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">100</td>
+                                </tr>
+                            </tfoot> */}
+
+                        </table>
+                    </div>
+                </div>
+             </div>
+        </>
+        
+        )
+
+}
