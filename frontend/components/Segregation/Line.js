74e454b9dbba6c0cd41bb16f94780991feb9b3e1
Theo Kaufman <tpkaufman@gmail.com>
2022-10-06 08:46:21 -0400

    Testing Line function

--- frontend/components/Segregation/Line.js
+++ frontend/components/Segregation/Line.js
@@ -6,6 +6,10 @@ import { years } from "../SelectOptions";
 
 export default function LineGraph ({linedata}) {
 
+    const test = [
+        {x: 2001, y: .5},
+        {x: 2002, y: .6}
+    ]
     
     const makeLines = () => { 
         // { linedata.length >0 &&
@@ -38,8 +42,25 @@ export default function LineGraph ({linedata}) {
             domain={{x: [2000, 2021], y: [0, 1]}} 
             width={1000}
             height={400}>
-
-            {makeLines()}
+{/* 
+            {makeLines()} */}
+             <VictoryLine 
+                    style={{
+                        data: { stroke: "#c43a31" },
+                        parent: { border: "1px solid #ccc"},
+                        labels: {display: "none"}
+                    }}
+                    data={linedata.data}
+                    />
+                
+                <VictoryLine 
+                    style={{
+                        data: { stroke: "#c43a31" },
+                        parent: { border: "1px solid #ccc"},
+                        labels: {display: "none"}
+                    }}
+                    data={test}
+                    />
 
             </VictoryChart>
         </div>
