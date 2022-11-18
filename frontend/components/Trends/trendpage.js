834f20e5f380b729ec48dcbcecd4a7f66afe7997
Theo Kaufman <tpkaufman@gmail.com>
2022-11-18 17:43:59 -0500

    Changes to trend page layout

--- frontend/components/Trends/trendpage.js
+++ frontend/components/Trends/trendpage.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AreaChart from "./Area";
 import TrendTable from "./TrendTable";
+import GradeLines from "./GradeLine";
 
 
 export default function Trends ({TrendData, id, title}) {
@@ -9,21 +10,28 @@ export default function Trends ({TrendData, id, title}) {
     return (
         <>
         <div className="flex flex-row">
-            <span className="text-4xl">Demographic Trends for <b>{title}</b></span>
+            <span className="text-2xl">Demographic Trends</span>
         </div>
 
 
-        <div className="pt-3">
+        <div className="flex flex-wrap justify-apart h-full">
+        <div className="w-1/2 h-full pt-3">
             <AreaChart TrendData={TrendData}/>
-        </div>
+         </div>
 
+        {/*
         <div className="pt-3">
             <span className="text-4xl">Enrollment Trends by Grade</span>
+        </div>*/}
+
+        <div className="w-1/2 h-full py-3"> 
+            <GradeLines TrendData={TrendData}/>
+        </div>
         </div>
         
-        <div className="py-3">
+        {/* <div className="py-3">
             <TrendTable TrendData={TrendData}/>
-        </div>
+        </div> */}
 
         
         </>
