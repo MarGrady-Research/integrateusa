8c0c4feb334e4708cc405a6078afb00faefe1be2
Theo Kaufman <tpkaufman@gmail.com>
2023-01-24 18:34:47 -0500

    wsgi config

--- wsgi-entrypoint.sh
+++ wsgi-entrypoint.sh
@@ -8,4 +8,4 @@ done
 ../manage.py migrate
 ../manage.py collectstatic --noinput
 
-cd .. && gunicorn --bind 0.0.0.0:8000 integrateusa.wsgi:application --workers 4 --threads 4
+cd .. && gunicorn --bind 0.0.0.0:8000 integrateusa.wsgi:application --workers 3 --threads 3 --max-requests 1000 --max-worker-lifetime 3600 --reload-on-rss 256 --worker-reload-mercy 60
