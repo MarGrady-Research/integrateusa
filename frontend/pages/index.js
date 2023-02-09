ce8ae2726f9c6fae0f068f4b20efb8b166a6f3d3
Theo Kaufman <tpkaufman@gmail.com>
2023-02-09 12:50:44 -0500

    Scrollytelling progress

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -11,36 +11,12 @@ export default function Home() {
       <Head>
         <title>Integrate USA</title>
         <meta name="description" content="Exploring school segregation" />
-        <link rel="icon" href="/mg_logo_cropped.png" />
+        <link rel="icon" href="/Image Only.png" />
       </Head>
 
-      <main className={styles.main}>
-        
-        {/* <div className='flex flex-col items-center mx-auto'>
-          
-        <div className='inline-flex items-center'>
-        <Image src="/mg_logo_cropped.png" 
-                        alt="margrady logo"
-                        width = {100}
-                        height={80}/>
-        <span className="ml-3 text-3xl font-raleway">IntegrateUSA</span>
-        </div>
-
-        <Link href='/info'>
-        <span className='inline-flex items-center hover:text-gray-500 hover:cursor-pointer font-raleway py-5'>Explore the dashboard <ChevronDoubleRightIcon className='h-5 w-5'/></span> 
-        </Link>
-  
-        <div className='py-5 flex items-center'>
-        <span className='font-raleway'>Or scroll through the story </span>
-        <div>
-        <ChevronDoubleDownIcon className='w-5 h-5'/>
-        </div>
-        </div>
-
-        </div> */}
+      {/* <main className={styles.main}> */}
         <Scroller />
-
-      </main> 
+      {/* </main>  */}
     </div>
   )
 }
