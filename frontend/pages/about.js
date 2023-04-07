d0b468a6d0bae2d83e1533ce77da353683278f13
Jesse Margolis <jesse@margrady.com>
2023-04-07 10:58:12 -0400

    text change to about page

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -33,11 +33,28 @@ export default function About(){
             <div className="justify-center flex">
             <div className="w-4/5 flex pt-5">
                 <span className="text-lg text-justify font-raleway pl-5 h-full">
-                    IntegrateUSA is a project by <a href="http://margrady.com" className="text-blue-800">MarGrady Research</a>, a mission-driven consulting firm specializing in education projects.
-                    The project collects, standardizes and visualizes publicly available enrollment data across the US from 2000 to present,
-                    providing a demographic overview of School Districts, Counties and States, as well as measures of segregation at each level.
-
-                    It's a really good project.
+                    IntegrateUSA is a project by <a href="http://margrady.com" className="text-blue-800" target = "_blank">MarGrady Research</a>, a mission-driven consulting firm 
+                    specializing in education projects. The project collects, standardizes and visualizes publicly available enrollment data across the US from 
+                    2000 to present, providing a demographic overview of school districts, counties, and states, as well as measures of segregation at each level.
+                    <br></br>
+                    <br></br>
+                    IntegrateUSA is primarily based on data from the National Center for Education Statistics' <a href="https://nces.ed.gov/ccd/" 
+                    className="text-blue-800" target = "_blank"> Common Core of Data.</a> The data have been cleaned to standardize school codes and names over time and
+                    exclude correctional facilities, district offices, hospital/homebound students, and schools that have fewer than 25 students in all years.
+                    Charter schools have been assigned to local school districts based on their geographic location.
+                    <br></br>
+                    <br></br>
+                    The dashboard measures segregation using the normalized exposure index. This measure compares the average demographics of a school for a 
+                    student of one race or ethnicity within a geographic area (e.g. district, county, or state) to the average demographics of a school for a student  
+                    who is not of that race or ethnicty within that same geographic area. This measure is adapted from a measure described by researchers from the
+                    University of Southern California and Stanford as part of the <a href="https://socialinnovation.usc.edu/segregation/" className="text-blue-800" 
+                     target = "_blank"> Segregation Index project. </a>
+                    <br></br>
+                    <br></br>
+                    We would like to thank the Carnegie Corporation of New York for its support in making this project possible.
+                    <br></br>
+                    <br></br>
+                    If you have any questions, please contact MarGrady Research at integrateusa@margrady.com.
                 </span> 
             </div>
             </div>
