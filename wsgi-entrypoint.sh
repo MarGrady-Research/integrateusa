4a0202721ee415c823a12c47d1bd992f6a788f71
Theo Kaufman <tpkaufman@gmail.com>
2023-04-10 17:31:57 -0400

    Config Updates

--- wsgi-entrypoint.sh
+++ wsgi-entrypoint.sh
@@ -8,4 +8,4 @@ done
 ../manage.py migrate
 ../manage.py collectstatic --noinput
 
-cd .. && gunicorn --bind 0.0.0.0:8000 integrateusa.wsgi:application --workers 2 --max-requests 500 --max-requests-jitter 50
+cd .. && gunicorn --bind 0.0.0.0:8000 integrateusa.wsgi:application --workers 1 --max-requests 500 --max-requests-jitter 50
