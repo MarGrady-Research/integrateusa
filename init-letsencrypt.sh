584555c554fbc909271f5032a3a7580c93c63b85
Theo Kaufman <tpkaufman@gmail.com>
2023-04-10 15:06:56 -0400

    production config changes

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(boilerplate.saasitive.com www.boilerplate.saasitive.com)
+domains=(integrateusa.org www.integrateusa.org)
 rsa_key_size=4096
 data_path="./nginx/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="theo@margrady.com" # Adding a valid address is strongly recommended
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
