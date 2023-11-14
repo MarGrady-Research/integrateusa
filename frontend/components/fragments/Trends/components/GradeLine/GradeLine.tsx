fcb72ea1517f3d41025da62b5c9097565a631503
Mrinal Mech <mrinalmech@gmail.com>
2023-11-14 21:55:24 +0530

    Fix

---
+++ frontend/components/fragments/Trends/components/GradeLine/GradeLine.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function GradeLines({ TrendData, grade }) {
+  const grades = [
+    "PK",
+    "KG",
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+    "UG",
+  ];
+
+  let sortedData = TrendData.sort((a, b) => {
+    return a["year"] - b["year"];
+  });
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+    },
+    scales: {
+      y: {
+        position: "right",
+      },
+    },
+  };
+
+  const labels = [...new Set(sortedData.map((e) => e.year))];
+
+  const makeLines = () => {
+    return grades.map((el) => {
+      return {
+        label: el,
+        data: sortedData
+          .filter((e) => e.grade === el)
+          .map((e) => e.asian + e.black + e.hispanic + e.other + e.white),
+        borderColor: el === grade ? "rgb(255, 99, 132)" : "rgb(169, 169, 169)",
+        backgroundColor:
+          el === grade ? "rgb(255, 99, 132)" : "rgb(169, 169, 169)",
+      };
+    });
+  };
+
+  const data = {
+    labels,
+    datasets: makeLines(),
+  };
+
+  return <>{TrendData && <Line options={options} data={data} />}</>;
+}
---
+++ frontend/components/fragments/Trends/components/TableYearGrade/TableYearGrade.tsx
@@ -0,0 +1,63 @@
+// JLM This is copied from the GradeLines.js and SchoolLevelTable.js
+
+import React from "react";
+import { yearsData } from "../../../Selection/data";
+import { gradesTableData } from "../../data";
+
+export default function TableYearGrade({ TrendData }) {
+  /* tableHeader: function to create header row across the top of the table */
+  const tableHeader = (e) => (
+    <tr>
+      <th scope="col" className="school-ch"></th>
+      {e.map((element) => (
+        <th scope="col" className="school-th" key={element.value}>
+          {" "}
+          {element.label}{" "}
+        </th>
+      ))}
+    </tr>
+  );
+
+  /* tableBody: function to create rows and columns for body of table */
+  const tableBody = (e, f) =>
+    e.map((elementRow) => (
+      <tr key={elementRow.value}>
+        <td className="school-ch"> {elementRow.label} </td>
+        {f.map((elementCol) => (
+          <td className="school-td" key={elementCol.value}>
+            {" "}
+            {TrendData.filter(
+              (e) => e.year === elementRow.value && e.grade === elementCol.value
+            )[0]
+              ? TrendData.filter(
+                  (e) =>
+                    e.year === elementRow.value && e.grade === elementCol.value
+                )[0]["total"].toLocaleString()
+              : "-"}
+          </td>
+        ))}
+      </tr>
+    ));
+  /* return statement, which calls the functions */
+  return (
+    <div className="mt-2 container flex flex-col">
+      <div className="-my-2 overflow-x-auto -mx-4 sm:-mx-2 lg:-mx-4">
+        <div className="py-2 align -middle inline-block min-w-full">
+          <div className="shadow overflow-hidden border border-gray-200">
+            <div className="overflow-x-auto">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-200">
+                  {tableHeader(gradesTableData)}
+                </thead>
+
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {tableBody(yearsData, gradesTableData)}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
