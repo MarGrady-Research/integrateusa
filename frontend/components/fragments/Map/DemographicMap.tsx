df36f335b2012eeb0ba902d69a2ec84effe1d4d8
Mrinal Mech <mrinalmech@gmail.com>
2023-11-19 04:45:38 +0530

    Add ViewDialog

--- frontend/components/fragments/Map/DemographicMap.tsx
+++ frontend/components/fragments/Map/DemographicMap.tsx
@@ -11,42 +11,8 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import mapbox_token from "../../../Key";
 import MapPie from "./components/MapPies";
 import Slideover from "./components/Slideover";
-import SummaryPie from "./components/SummaryPie";
 import AreaPie from "./components/AreaPie";
-
-const getStudentInfo = (renderedFeatures) => {
-  let studentsTotal = 0;
-  let asianTotal = 0;
-  let blackTotal = 0;
-  let hispanicTotal = 0;
-  let whiteTotal = 0;
-  let otherTotal = 0;
-
-  for (let feature of renderedFeatures) {
-    studentsTotal += feature.properties.tot_enr;
-    asianTotal += feature.properties.as;
-    blackTotal += feature.properties.bl;
-    hispanicTotal += feature.properties.hi;
-    whiteTotal += feature.properties.wh;
-    otherTotal += feature.properties.or;
-  }
-
-  const studentsEnrolled = studentsTotal.toLocaleString();
-  const asianPercentage = ((asianTotal / studentsTotal) * 100).toFixed(1);
-  const blackPercentage = ((blackTotal / studentsTotal) * 100).toFixed(1);
-  const hispanicPercentage = ((hispanicTotal / studentsTotal) * 100).toFixed(1);
-  const whitePercentage = ((whiteTotal / studentsTotal) * 100).toFixed(1);
-  const otherPercentage = ((otherTotal / studentsTotal) * 100).toFixed(1);
-
-  return {
-    studentsEnrolled,
-    asianPercentage,
-    blackPercentage,
-    hispanicPercentage,
-    whitePercentage,
-    otherPercentage,
-  };
-};
+import ViewDialog from "./components/ViewDialog";
 
 export default function DemographicMap({ mapData }) {
   const [cursor, setCursor] = useState("auto");
@@ -267,15 +233,6 @@ export default function DemographicMap({ mapData }) {
 
   let layerProp = () =>
     clickInfo.feature.properties.STUSPS ? "STUSPS" : "GEOID";
-
-  const {
-    studentsEnrolled,
-    asianPercentage,
-    blackPercentage,
-    hispanicPercentage,
-    whitePercentage,
-    otherPercentage,
-  } = getStudentInfo(renderedFeatures);
 
   return (
     <div className="relative w-full h-[calc(100vh-66px)]">
@@ -605,58 +562,7 @@ export default function DemographicMap({ mapData }) {
             )}
           </div>
         )}
-        <div className="absolute bottom-10 left-10 bg-gray-900 text-white text-center font-light w-60 h-72 rounded-md hidden lg:block">
-          <div className="p-3">
-            {renderedFeatures.length === 0 ? (
-              <span className="italic">
-                Zoom or drag the map to see school data here!
-              </span>
-            ) : (
-              <>
-                <span>
-                  <b>Schools in View: </b>
-                  {renderedFeatures
-                    ? renderedFeatures.length.toLocaleString()
-                    : 0}
-                </span>
-                <br />
-                <span>
-                  <b>Students Enrolled: </b>
-                  {studentsEnrolled}
-                </span>
-                <br />
-                <span className="text-asian">
-                  <b>Asian:</b>{" "}
-                  <span className="text-white">{asianPercentage}%</span>
-                </span>
-                <br />
-                <span className="text-blackstudents">
-                  <b>Black:</b>{" "}
-                  <span className="text-white">{blackPercentage}%</span>
-                </span>
-                <br />
-                <span className="text-hispanic">
-                  <b>Hispanic:</b>{" "}
-                  <span className="text-white">{hispanicPercentage}%</span>
-                </span>
-                <br />
-                <span className="text-whitestudents">
-                  <b>White:</b>{" "}
-                  <span className="text-white">{whitePercentage}%</span>
-                </span>
-                <br />
-                <span className="text-other">
-                  <b>Other:</b>{" "}
-                  <span className="text-white">{otherPercentage}%</span>
-                </span>
-                <br />
-                <div className="w-1/2 justify-center pt-2 mx-auto">
-                  <SummaryPie renderedFeatures={renderedFeatures} />
-                </div>
-              </>
-            )}
-          </div>
-        </div>
+        <ViewDialog renderedFeatures={renderedFeatures} />
       </Map>
       <Slideover
         handleVisibility={handleVisibility}
---
+++ frontend/components/fragments/Map/components/ViewDialog/ViewDialog.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+
+import SummaryPie from "../SummaryPie";
+
+const getStudentInfo = (renderedFeatures) => {
+  let studentsTotal = 0;
+  let asianTotal = 0;
+  let blackTotal = 0;
+  let hispanicTotal = 0;
+  let whiteTotal = 0;
+  let otherTotal = 0;
+
+  for (let feature of renderedFeatures) {
+    studentsTotal += feature.properties.tot_enr;
+    asianTotal += feature.properties.as;
+    blackTotal += feature.properties.bl;
+    hispanicTotal += feature.properties.hi;
+    whiteTotal += feature.properties.wh;
+    otherTotal += feature.properties.or;
+  }
+
+  const studentsEnrolled = studentsTotal.toLocaleString();
+  const asianPercentage = ((asianTotal / studentsTotal) * 100).toFixed(1);
+  const blackPercentage = ((blackTotal / studentsTotal) * 100).toFixed(1);
+  const hispanicPercentage = ((hispanicTotal / studentsTotal) * 100).toFixed(1);
+  const whitePercentage = ((whiteTotal / studentsTotal) * 100).toFixed(1);
+  const otherPercentage = ((otherTotal / studentsTotal) * 100).toFixed(1);
+
+  return {
+    studentsEnrolled,
+    asianPercentage,
+    blackPercentage,
+    hispanicPercentage,
+    whitePercentage,
+    otherPercentage,
+  };
+};
+
+export default function ViewDialog({ renderedFeatures }) {
+  const {
+    studentsEnrolled,
+    asianPercentage,
+    blackPercentage,
+    hispanicPercentage,
+    whitePercentage,
+    otherPercentage,
+  } = getStudentInfo(renderedFeatures);
+
+  const noOfSchoolsInView = renderedFeatures.length.toLocaleString();
+  const areSchoolsPresentInView = renderedFeatures.length != 0;
+
+  return (
+    <div className="absolute bottom-10 left-10 bg-gray-900 text-white text-center font-light w-60 h-72 rounded-md hidden lg:block">
+      <div className="p-3">
+        {!areSchoolsPresentInView ? (
+          <span className="italic">
+            Zoom or drag the map to see school data here!
+          </span>
+        ) : (
+          <>
+            <span>
+              <b>Schools in View: </b>
+              {noOfSchoolsInView}
+            </span>
+            <br />
+            <span>
+              <b>Students Enrolled: </b>
+              {studentsEnrolled}
+            </span>
+            <br />
+            <span className="text-asian">
+              <b>Asian:</b>{" "}
+              <span className="text-white">{asianPercentage}%</span>
+            </span>
+            <br />
+            <span className="text-blackstudents">
+              <b>Black:</b>{" "}
+              <span className="text-white">{blackPercentage}%</span>
+            </span>
+            <br />
+            <span className="text-hispanic">
+              <b>Hispanic:</b>{" "}
+              <span className="text-white">{hispanicPercentage}%</span>
+            </span>
+            <br />
+            <span className="text-whitestudents">
+              <b>White:</b>{" "}
+              <span className="text-white">{whitePercentage}%</span>
+            </span>
+            <br />
+            <span className="text-other">
+              <b>Other:</b>{" "}
+              <span className="text-white">{otherPercentage}%</span>
+            </span>
+            <br />
+            <div className="w-1/2 justify-center pt-2 mx-auto">
+              <SummaryPie renderedFeatures={renderedFeatures} />
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
