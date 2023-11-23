a7c30dd8c0cce8f670579abcf1e4d4eeff5b85b3
Mrinal Mech <mrinalmech@gmail.com>
2023-11-23 15:29:55 +0530

    Refactor scroller

---
+++ frontend/components/fragments/Scroll/Scroll.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+
+import Scroller from "./components/Scroller";
+
+function WelcomeScreen() {
+  return (
+    <div className="h-screen flex flex-col space-y-20 items-center justify-center text-xl">
+      <Link href="/info">
+        <Image
+          src="/IntegrateUSALogo.png"
+          alt="IntegrateUSA Logo"
+          width={300}
+          height={80}
+          className="hover:cursor-pointer"
+        />
+      </Link>
+      <Link href="/info">
+        <a className="hover:text-gray-500  inline-flex items-center">
+          Explore the dashboard
+          <KeyboardDoubleArrowRightIcon className="ml-2" fontSize="large" />
+        </a>
+      </Link>
+      <div className="flex flex-col items-center">
+        <p className="mb-4">
+          Scroll down for a case study of New York City&#39;s District 15
+        </p>
+        <KeyboardDoubleArrowDownIcon
+          fontSize="large"
+          className="animate-bounce"
+        />
+      </div>
+    </div>
+  );
+}
+
+function EndScreen() {
+  return (
+    <div className="sticky h-screen flex flex-col py-10 items-center justify-between text-xl">
+      <div className="px-4 text-center">
+        <p className="mb-4">
+          IntegrateUSA was built to explore segregation in districts, counties
+          and states nationwide
+        </p>
+        <p>
+          Use the dashboard to visualize demographics and understand segregation
+          levels in different areas over time
+        </p>
+      </div>
+
+      <Link href="/info">
+        <a className="hover:text-gray-500  inline-flex items-center">
+          Explore the dashboard
+          <KeyboardDoubleArrowRightIcon className="ml-2" fontSize="large" />
+        </a>
+      </Link>
+
+      <Link href="http://www.margrady.com/">
+        <a>
+          <Image
+            src="/mg-logo.png"
+            alt="MarGrady Logo"
+            width={350}
+            height={80}
+          />
+        </a>
+      </Link>
+    </div>
+  );
+}
+
+export default function Scroll() {
+  const [currentStepIndex, setCurrentStepIndex] = useState(null);
+
+  const onStepEnter = ({ data }) => {
+    setCurrentStepIndex(data);
+  };
+
+  const onStepExit = ({ data, direction }) => {
+    data === 0 && direction === "up" ? setCurrentStepIndex(-1) : null;
+  };
+
+  return (
+    <div className="font-sans">
+      <WelcomeScreen />
+      <Scroller
+        onStepEnter={onStepEnter}
+        onStepExit={onStepExit}
+        currentStepIndex={currentStepIndex}
+      />
+      <EndScreen />
+    </div>
+  );
+}
---
+++ frontend/components/fragments/Scroll/components/Scroller/Scroller.tsx
@@ -0,0 +1,204 @@
+import React from "react";
+import { Scrollama, Step } from "react-scrollama";
+
+import ScrollerPie from "./graphs/Graph1";
+import ScrollerBar from "./graphs/Graph2";
+import ScrollerBar2 from "./graphs/Graph3";
+import ScrollerBar3 from "./graphs/Graph4";
+import ScrollerLine from "./graphs/Graph5";
+import ScrollerLine2 from "./graphs/Graph6";
+import ScrollerLine3 from "./graphs/Graph7";
+import ScrollerLine4 from "./graphs/Graph8";
+import {
+  rawdata,
+  schooldata,
+  exposureData,
+  comparisonData,
+  d15ExposureWhiteV1,
+  d15ExposureWhiteV2,
+  fullCompData,
+  compDataNormalized,
+} from "./data";
+
+const charts = (currentStepIndex) => {
+  if (currentStepIndex === 0) {
+    return <ScrollerPie rawdata={rawdata} />;
+  }
+  if (currentStepIndex === 1) {
+    return <ScrollerBar schooldata={schooldata} />;
+  }
+  if (currentStepIndex === 2) {
+    return <ScrollerBar2 exposureData={exposureData} />;
+  }
+  if (currentStepIndex === 3) {
+    return <ScrollerBar3 comparisonData={comparisonData} />;
+  }
+  if (currentStepIndex === 4) {
+    return <ScrollerLine d15ExposureWhite={d15ExposureWhiteV1} />;
+  }
+  if (currentStepIndex === 5) {
+    return <ScrollerLine2 d15ExposureWhite={d15ExposureWhiteV2} />;
+  }
+  if (currentStepIndex === 6) {
+    return <ScrollerLine3 fullCompData={fullCompData} />;
+  }
+  if (currentStepIndex === 7) {
+    return <ScrollerLine4 compDataNormalized={compDataNormalized} />;
+  }
+};
+
+const stepStyle = {
+  margin: "0 auto 3rem auto",
+  paddingTop: "50%",
+  paddingBottom: "50%",
+};
+
+const graphicStyle = {
+  height: "60vh",
+  top: "20vh",
+};
+
+export default function Scroller({
+  onStepEnter,
+  onStepExit,
+  currentStepIndex,
+}) {
+  return (
+    <div className="p-20 flex justify-between">
+      <div className="basis-1/3 text-center text-xl">
+        <Scrollama
+          offset={0.5}
+          onStepEnter={onStepEnter}
+          onStepExit={onStepExit}
+        >
+          <Step data={0}>
+            <div style={stepStyle}>
+              <p className="mb-3">
+                In 2019, New York City&#39;s School District 15 was a racially
+                diverse district
+              </p>
+              <p className="mb-3">The 6th graders in the district were:</p>
+              <p className="text-hispanic font-semibold">41% Hispanic</p>
+              <p className="text-whitestudents font-semibold">30% White</p>
+              <p className="text-asian font-semibold">13% Asian</p>
+              <p className="text-blackstudents font-semibold">13% Black</p>
+              <p className="text-other font-semibold">4% Other Races</p>
+            </div>
+          </Step>
+          <Step data={1}>
+            <div style={stepStyle}>
+              <p className="mb-3">
+                But when we look at District 15 on a school level, White
+                students appear to be segregated from non-White students
+              </p>
+              <p>
+                In 2019,{" "}
+                <span className="text-whitestudents font-semibold">
+                  2/3 of White students
+                </span>{" "}
+                in the district attend just <b>5</b> of the district&#39;s{" "}
+                <b>16</b> middle schools
+              </p>
+            </div>
+          </Step>
+          <Step data={2}>
+            <div style={stepStyle}>
+              <p className="mb-3">
+                One way to measure segregation is using{" "}
+                <b>Normalized Exposure</b> rates
+              </p>
+              <p>
+                For example, we can compare the share of{" "}
+                <span className="text-whitestudents">White</span> students in
+                the average <span className="text-whitestudents">White</span>{" "}
+                student&#39;s school to the share of{" "}
+                <span className="text-whitestudents">White</span> students in
+                the average non-
+                <span className="text-whitestudents">White</span> student&#39;s
+                school
+              </p>
+            </div>
+          </Step>
+          <Step data={3}>
+            <div style={stepStyle}>
+              <p className="mb-3">
+                In District 15 in 2019, the average White student&#39;s school
+                had <b>42%</b> White students
+              </p>
+              <p className="mb-3">
+                The average non-White student&#39;s school had <b>24%</b> White
+                students
+              </p>
+              <p>
+                The difference of these shares is the <b>Normalized Exposure</b>{" "}
+                rate: <b>18%</b>
+              </p>
+            </div>
+          </Step>
+          <Step data={4}>
+            <div style={stepStyle}>
+              <p>
+                Normalized Exposure rates can help us to understand segregation
+                in the district over time
+              </p>
+            </div>
+          </Step>
+          <Step data={5}>
+            <div style={stepStyle}>
+              <p className="mb-3">
+                In 2019,{" "}
+                <span className="text-line-red">
+                  <b>District 15</b>
+                </span>{" "}
+                implemented an integration plan
+              </p>
+              <p>
+                After the plan&#39;s implementation, we see a dropoff in
+                Normalized Exposure rates for White students in the District
+              </p>
+            </div>
+          </Step>
+          <Step data={6}>
+            <div style={stepStyle}>
+              <p className="mb-3">
+                We can compare{" "}
+                <span className="text-line-red">
+                  <b>District 15</b>
+                </span>{" "}
+                to demographically similar districts without integration plans
+              </p>
+              <p>
+                The Normalized Exposure for White students in{" "}
+                <span className="text-line-red">
+                  <b>District 15</b>
+                </span>{" "}
+                shows a steep drop compared to other districts
+              </p>
+            </div>
+          </Step>
+          <Step data={7}>
+            <div style={stepStyle}>
+              <p className="mb-3">
+                We can also normalize these rates using the 2019 values for each
+                district
+              </p>
+              <p>
+                <span className="text-line-red">
+                  <b>District 15</b>
+                </span>{" "}
+                has the largest drop in White Normalized Exposure rates of any
+                comparable district after 2019
+              </p>
+            </div>
+          </Step>
+        </Scrollama>
+      </div>
+      <div
+        style={graphicStyle}
+        className="basis-2/3 sticky flex justify-center items-center"
+      >
+        <div className="w-1/2">{charts(currentStepIndex)}</div>
+      </div>
+    </div>
+  );
+}
