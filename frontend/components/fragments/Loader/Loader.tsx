a114b947945503b90e3e5a0796131e14caf4b9b0
Mrinal Mech <mrinalmech@gmail.com>
2023-11-27 00:23:38 +0530

    Loader fix

--- frontend/components/fragments/Loader/Loader.tsx
+++ frontend/components/fragments/Loader/Loader.tsx
@@ -1,29 +1,124 @@
 import React from "react";
 
-export default function Loader({ text = "Loading...", noText = false }) {
+// @ts-ignore
+import { container } from "./Loader.module.scss";
+
+export default function Loader() {
   return (
-    <div className="flex flex-col justify-center items-center text-xl font-light">
+    <div className="flex justify-center">
       <svg
-        className="animate-spin h-8 w-8 text-white"
+        width="179"
+        height="184"
         xmlns="http://www.w3.org/2000/svg"
-        fill="white"
-        viewBox="0 0 24 24"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        xmlSpace="preserve"
+        overflow="hidden"
       >
-        <circle
-          className="opacity-5"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="white"
-          strokeWidth="4"
-        ></circle>
-        <path
-          className="opacity-75"
-          fill="gray"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>{" "}
-      {!noText ? <div className="opacity-50 mt-4">{text}</div> : null}
+        <g transform="translate(-421 -1003)">
+          <g className={container}>
+            <path
+              d="M0 0 0.000360892 47.4267"
+              stroke="#ED7D31"
+              strokeWidth="9.16667"
+              strokeLinecap="butt"
+              strokeLinejoin="miter"
+              strokeMiterlimit="8"
+              strokeOpacity="1"
+              fill="none"
+              fill-rule="evenodd"
+              transform="matrix(1 0 0 -1 507.5 1056.93)"
+            />
+            <path
+              d="M424.5 1097.5 469.357 1097.5"
+              stroke="#ED7D31"
+              strokeWidth="9.16667"
+              strokeLinecap="butt"
+              strokeLinejoin="miter"
+              strokeMiterlimit="8"
+              strokeOpacity="1"
+              fill="none"
+              fill-rule="evenodd"
+            />
+            <path
+              d="M0 0 26.4 25.1766"
+              stroke="#4472C4"
+              strokeWidth="9.16667"
+              strokeLinecap="butt"
+              strokeLinejoin="miter"
+              strokeMiterlimit="8"
+              strokeOpacity="1"
+              fill="none"
+              fill-rule="evenodd"
+              transform="matrix(1 0 0 -1 537.5 1065.68)"
+            />
+            <path
+              d="M0 0 0.000360892 45.7449"
+              stroke="#ED7D31"
+              strokeWidth="9.16667"
+              strokeLinecap="butt"
+              strokeLinejoin="miter"
+              strokeMiterlimit="8"
+              strokeOpacity="1"
+              fill="none"
+              fill-rule="evenodd"
+              transform="matrix(1 0 0 -1 507.5 1182.24)"
+            />
+            <path
+              d="M544.5 1097.5 590.7 1097.5"
+              stroke="#ED7D31"
+              strokeWidth="9.16667"
+              strokeLinecap="butt"
+              strokeLinejoin="miter"
+              strokeMiterlimit="8"
+              strokeOpacity="1"
+              fill="none"
+              fill-rule="evenodd"
+            />
+            <path
+              d="M0 0 26.4751 26.4"
+              stroke="#4472C4"
+              strokeWidth="9.16667"
+              strokeLinecap="butt"
+              strokeLinejoin="miter"
+              strokeMiterlimit="8"
+              strokeOpacity="1"
+              fill="none"
+              fill-rule="evenodd"
+              transform="matrix(1 0 0 -1 453.5 1149.9)"
+            />
+            <path
+              d="M450.5 1040.5 476.9 1066.9"
+              stroke="#4472C4"
+              strokeWidth="9.16667"
+              strokeLinecap="butt"
+              strokeLinejoin="miter"
+              strokeMiterlimit="8"
+              strokeOpacity="1"
+              fill="none"
+              fill-rule="evenodd"
+            />
+            <path
+              d="M537.5 1123.5 564.746 1149.58"
+              stroke="#4472C4"
+              strokeWidth="9.16667"
+              strokeLinecap="butt"
+              strokeLinejoin="miter"
+              strokeMiterlimit="8"
+              strokeOpacity="1"
+              fill="none"
+              fill-rule="evenodd"
+            />
+          </g>
+          <g>
+            <path
+              d="M487 1096.5C487 1084.07 496.178 1074 507.5 1074 518.822 1074 528 1084.07 528 1096.5 528 1108.93 518.822 1119 507.5 1119 496.178 1119 487 1108.93 487 1096.5Z"
+              fill="#ED7D31"
+              fill-rule="evenodd"
+              fill-opacity="1"
+            />
+          </g>
+        </g>
+      </svg>
     </div>
   );
 }
