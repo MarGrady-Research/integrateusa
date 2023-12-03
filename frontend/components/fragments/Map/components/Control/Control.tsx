02a7f3175308deb9f7325246efbed9bb0b57d106
Mrinal Mech <mrinalmech@gmail.com>
2023-12-03 23:13:42 +0530

    Add radio buttons

--- frontend/components/fragments/Map/components/Control/Control.tsx
+++ frontend/components/fragments/Map/components/Control/Control.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+
 import Search from "../Search";
 
 import { Bounds, Level } from "../../../../../interfaces";
+
 import {
   asianColor,
   blackColor,
@@ -44,32 +51,37 @@ export default function Control({ handleVisibility, handleBounds }: Props) {
     ));
 
   const radio = (l: Level, label?: string) => (
-    <div className="inline-flex">
-      <label className="inline-flex items-center">
-        <input
-          type="radio"
-          name={Level[l]}
-          value={l}
-          checked={level === l}
-          onChange={handleChange}
-          className="w-4 h-4 mr-2"
-        />
-        {label || Level[l]}
-      </label>
-    </div>
+    <FormControlLabel
+      value={l}
+      control={<Radio classes={{ root: "!p-0 !mr-1.5" }} size="small" />}
+      label={label || Level[l]}
+      classes={{ root: "!-ml-0.5" }}
+    />
   );
+
   const boundaries = () => (
     <>
-      <p className="text-lg text-gray-900 mb-1">Boundaries</p>
-      <div className="flex flex-col mb-4">
-        {radio(Level.School, "No Boundary")}
-        {radio(Level.District)}
-        {radio(Level.County)}
-        {radio(Level.State)}
-      </div>
-      <div>
-        <Search level={level} handleBounds={handleBounds} />
-      </div>
+      <FormControl>
+        <FormLabel
+          id="level-radio-buttons-group-label"
+          classes={{ root: "!text-lg !text-gray-900 !mb-1" }}
+        >
+          Boundaries
+        </FormLabel>
+        <RadioGroup
+          aria-labelledby="level-radio-buttons-group-label"
+          value={level}
+          name="level-radio-buttons-group"
+          onChange={handleChange}
+          classes={{ root: "mb-4" }}
+        >
+          {radio(Level.School, "No Boundary")}
+          {radio(Level.District)}
+          {radio(Level.County)}
+          {radio(Level.State)}
+        </RadioGroup>
+      </FormControl>
+      <Search level={level} handleBounds={handleBounds} />
     </>
   );
 
