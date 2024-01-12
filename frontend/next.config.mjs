2bbee6d6f78e52b902a6ac9296bdbec740b36230
Mrinal Mech <mrinalmech@gmail.com>
2024-01-12 07:40:39 +0530

    Next Config mjs

---
+++ frontend/next.config.mjs
@@ -0,0 +1,22 @@
+export default () => {
+  /** @type {import('next').NextConfig} */
+
+  const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+      removeConsole: process.env.NODE_ENV === "production",
+    },
+  };
+
+  return {
+    ...nextConfig,
+    webpack: (config) => {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+      return config;
+    },
+  };
+};
