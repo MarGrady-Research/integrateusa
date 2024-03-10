757b0cbbd3096102a15541a1f43ea8b4d8162a43
Mrinal Mech <mrinalmech@gmail.com>
2024-03-10 12:12:39 +0530

    Hover on research article

--- frontend/components/fragments/Research/Research.tsx
+++ frontend/components/fragments/Research/Research.tsx
@@ -13,7 +13,10 @@ export default function Research() {
       <h2 className="text-lg mb-10">
         Publications, projects, and other resources appear below.
       </h2>
-      <div className="border-2 border-black p-5 flex flex-col items-center md:flex-row md:items-start">
+      <Link
+        href="/research/district15"
+        className="border-2 border-black hover:border-primary hover:bg-slate-50 duration-300 p-5 flex flex-col items-center md:flex-row md:items-start"
+      >
         <Image
           src={district15Img}
           objectFit="contain"
@@ -25,26 +28,16 @@ export default function Research() {
         />
         <div className="mt-4 md:mt-0 md:ml-4">
           <h3 className="text-xl font-semibold">
-            <Link href="/research/district15" className="hover:text-primary">
-              Case Study: Middle School Integration in New York City’s District
-              15
-            </Link>
+            Case Study: Middle School Integration in New York City’s District 15
           </h3>
           <h4 className="text-lg font-medium">Jesse Margolis</h4>
           <h4 className="text-lg font-medium mb-2">March 2024</h4>
           <p className="text-lg">
-            Using data from IntegrateUSA, this{" "}
-            <Link
-              href="/research/district15"
-              className="text-primary hover:underline underline-offset-4"
-            >
-              case study
-            </Link>{" "}
-            documents the impact of a middle school integration plan in District
-            15 in New York City.
+            Using data from IntegrateUSA, this case study documents the impact
+            of a middle school integration plan in District 15 in New York City.
           </p>
         </div>
-      </div>
+      </Link>
     </>
   );
 }
